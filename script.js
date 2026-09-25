64d628af633e2876ace957733a2380e3004742a1
Vineethm04 <vineethm412004@gmail.com>
2026-09-25 22:55:03 +0530

    Add dynamic categories and style category cards

---
+++ script.js
@@ -0,0 +1,19 @@
+const categoriesContainer = document.getElementById("categories-container");
+
+fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
+    .then(response => response.json())
+    .then(data => {
+
+    data.categories.forEach(category => {
+
+        const categoryCard = document.createElement("div");
+
+        categoryCard.innerHTML = `
+            <h3>${category.strCategory}</h3>
+        `;
+
+        categoriesContainer.appendChild(categoryCard);
+
+    });
+
+});
