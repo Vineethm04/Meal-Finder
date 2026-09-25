ec2e1887d71cb5d8bcd24b575aaade68f882d0cf
Vineethm04 <vineethm412004@gmail.com>
2026-09-25 23:29:03 +0530

    Add category filtering API

--- script.js
+++ script.js
@@ -4,17 +4,27 @@ fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
     .then(response => response.json())
     .then(data => {
 
-    data.categories.forEach(category => {
+        data.categories.forEach(category => {
 
-        const categoryCard = document.createElement("div");
+            const categoryCard = document.createElement("div");
 
-        categoryCard.innerHTML = `
-    <img src="${category.strCategoryThumb}" alt="${category.strCategory}">
-    <h3>${category.strCategory}</h3>
-`;
+            categoryCard.innerHTML = `
+                <img src="${category.strCategoryThumb}" alt="${category.strCategory}">
+                <h3>${category.strCategory}</h3>
+            `;
 
-        categoriesContainer.appendChild(categoryCard);
+            categoriesContainer.appendChild(categoryCard);
+
+            categoryCard.addEventListener("click", () => {
+
+                fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${category.strCategory}`)
+                    .then(response => response.json())
+                    .then(data => {
+                        console.log(data);
+                    });
+
+            });
+
+        });
 
     });
-
-});
