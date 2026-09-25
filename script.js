e684bffa178b9133824ec7c9aa6b2526606ab870
Vineethm04 <vineethm412004@gmail.com>
2026-09-25 23:49:45 +0530

    Display meals by category

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const categoriesContainer = document.getElementById("categories-container");
 
+const mealsContainer = document.getElementById("meals-container");
+
+
 fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
     .then(response => response.json())
     .then(data => {
@@ -15,12 +18,28 @@ fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
 
             categoriesContainer.appendChild(categoryCard);
 
+
             categoryCard.addEventListener("click", () => {
 
                 fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${category.strCategory}`)
                     .then(response => response.json())
                     .then(data => {
-                        console.log(data);
+
+                        mealsContainer.innerHTML = "";
+
+                        data.meals.forEach(meal => {
+
+                            const mealCard = document.createElement("div");
+
+                            mealCard.innerHTML = `
+                                <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
+                                <h3>${meal.strMeal}</h3>
+                            `;
+
+                            mealsContainer.appendChild(mealCard);
+
+                        });
+
                     });
 
             });
